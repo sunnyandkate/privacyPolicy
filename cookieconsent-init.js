e2f78c5cecde35dd98a766a098937829a8b06683
kate <77579746+sunnyandkate@users.noreply.github.com>
2023-08-11 17:21:10 +0200

    Update cookieconsent-init.js

--- cookieconsent-init.js
+++ cookieconsent-init.js
@@ -2,7 +2,7 @@
 var cc = initCookieConsent();
 
 // example logo
-var logo = '<img src="https://fontmeme.com/permalink/220805/3b90158d204fdd325173a5c7873e7cc1.png" alt="Logo" loading="lazy" style="margin-left: -4px; margin-bottom: -5px; height: 35px">';
+var logo = '';
 var cookie = '🍪';
 
 // run plugin with config object
